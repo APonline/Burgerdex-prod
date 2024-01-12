d3fa858df15d4944fb8c8343b3a36eba62c5fab7
apanemia <andrewgphillips74@gmail.com>
2024-01-12 00:11:58 -0500

    porting over from prod server repo

--- template/footer.php
+++
@@ -1,6 +0,0 @@
-		</section>
-		
-		<script src="<?php echo SITE_PATH_STATIC; ?>/assets/js/common/main.js"></script>
-		<script src="<?php echo SITE_PATH_STATIC; ?>/assets/js/lib/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
-	</body>
-</html>
--- view/user/user.logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-\Fr\LS::logout();
-
-?>
